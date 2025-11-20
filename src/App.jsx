0ea2f99181bfa01a0f5896120601a895049564d4
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-11-20 23:02:22 +0300

    useState Method

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,43 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { sculptureList } from './data';
-function handleNextClick() {
-  alert("Next");
-}
-function handlePreviousClick() {
-  alert("Previous");
-}
+
 function App() {
-  const index = 0;
+  const [index, setIndex] = useState(0);
   const sculpture = sculptureList[index];
+  const [showMore,setShowMore]=useState(false);
+  function handleNextClick() {
+    if (index < sculptureList.length - 1) {
+      setIndex(index + 1);
+    }
+    else {
+      setIndex(0);
+    }
+  }
+  function handlePreviousClick() {
+    if (index > 0) {
+      setIndex(index - 1);
+    }
+    else{
+      setIndex(sculptureList.length-1);
+    }
+
+  }
+  function handleShowClick(){
+    setShowMore(!showMore);
+  }
   return (
     <>
-      <button onClick={handleNextClick}>Next</button>
       <button onClick={handlePreviousClick}>Previous</button>
+      <button onClick={handleNextClick}>Next</button>
       <h2>
         <i>{sculpture.name} by {sculpture.artist}</i>
       </h2>
       <h3>{index + 1} of {sculptureList.length}</h3>
       <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
+      <p></p>
+      <button onClick={handleShowClick}> {showMore?"Hide":"Show"} Details</button>
+      {showMore&& <p>{sculpture.description}</p>}
     </>
   )
 }
