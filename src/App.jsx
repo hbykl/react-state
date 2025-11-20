7762f0bb1509456595a14fc562e6bf68b909b1df
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-11-20 22:36:58 +0300

    add data.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { sculptureList } from './data';
+function handleNextClick() {
+  alert("Next");
+}
+function handlePreviousClick() {
+  alert("Previous");
+}
 function App() {
-  const [count, setCount] = useState(0)
-
+  const index = 0;
+  const sculpture = sculptureList[index];
   return (
-   <p></p>
+    <>
+      <button onClick={handleNextClick}>Next</button>
+      <button onClick={handlePreviousClick}>Previous</button>
+      <h2>
+        <i>{sculpture.name} by {sculpture.artist}</i>
+      </h2>
+      <h3>{index + 1} of {sculptureList.length}</h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>{sculpture.description}</p>
+    </>
   )
 }
 
